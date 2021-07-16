9fdea53b6572858d0dd54fbf54f4891cf02e7ee1
HyeonjinChoi <63872787+HyeonjinChoi@users.noreply.github.com>
2021-07-16 16:47:46 +0900

    code: Update BOJ 11437

--- week03/assignment02/11437_LCA_HyeonjinChoi.go
+++ week03/assignment02/11437_LCA_HyeonjinChoi.go
@@ -33,19 +33,14 @@ type Tree struct {
 	linkedNode []int
 }
 
-// 정점의 부모 정점과 깊이를 저장하기 위한 구조체
-type Node struct {
-	parentNode int
-	depth      int
-}
-
 var (
-	sc       *bufio.Scanner
-	wr       *bufio.Writer
-	nodeNum  int
-	treeArr  []Tree
-	nodeArr  []Node
-	checkArr []bool
+	sc        *bufio.Scanner
+	wr        *bufio.Writer
+	nodeNum   int
+	treeArr   []Tree
+	depthArr  []int
+	checkArr  []bool
+	parentArr [][]int
 )
 
 func init() {
@@ -82,11 +77,18 @@ func setting() {
 // BFS 알고리즘을 이용하여 모든 정점의 부모 정점와 깊이를 구하는 함수
 func findParentAndDepth() {
 	q := Queue{}
-	nodeArr = make([]Node, nodeNum+1)
+	depthArr = make([]int, nodeNum+1)
 	checkArr = make([]bool, nodeNum+1)
+	parentArr = make([][]int, nodeNum+1)
+	for i := 0; i < nodeNum+1; i++ {
+		parentArr[i] = make([]int, 20)
+	}
+
 	q.Enqueue(1)
-	nodeArr[1] = Node{0, 0}
+	depthArr[1] = 0
 	checkArr[1] = true
+	// parentArr[i][j] : i번 정점의 2^j번째 부모 정점
+	parentArr[1][0] = 1
 
 	for {
 		if q.IsEmpty() {
@@ -98,9 +100,17 @@ func findParentAndDepth() {
 		for _, node := range treeArr[curNode].linkedNode {
 			if !checkArr[node] {
 				q.Enqueue(node)
-				nodeArr[node] = Node{curNode, nodeArr[curNode].depth + 1}
+				depthArr[node] = depthArr[curNode] + 1
 				checkArr[node] = true
+				parentArr[node][0] = curNode
 			}
+		}
+	}
+
+	for i := 1; i < 20; i++ {
+		for j := 1; j < nodeNum+1; j++ {
+			// 어떤 정점의 2^i번째 부모 정점은, 그 정점의 2^(i-1)번째 부모 정점의 2^(i-1)번째 부모 정점
+			parentArr[j][i] = parentArr[parentArr[j][i-1]][i-1]
 		}
 	}
 }
@@ -116,31 +126,32 @@ func printCommonParent() {
 
 // 두 정점 각각의 부모 정점을 비교하여 공통 조상을 찾는 함수
 func compareParent(node1, node2 int) int {
-	newNode1, newNode2 := compareDepth(node1, node2)
-
-	for {
-		if newNode1 == newNode2 {
-			return newNode1
-		} else {
-			newNode1 = nodeArr[newNode1].parentNode
-			newNode2 = nodeArr[newNode2].parentNode
-		}
-	}
-}
-
-// 두 정점 각각의 깊이를 비교하여 더 높은 정점이 낮은 정점의 깊이만큼 이동하는 함수
-func compareDepth(node1, node2 int) (int, int) {
 	newNode1, newNode2 := node1, node2
 
-	if nodeArr[node1].depth > nodeArr[node2].depth {
-		for i := 0; i < nodeArr[node1].depth-nodeArr[node2].depth; i++ {
-			newNode1 = nodeArr[newNode1].parentNode
-		}
-	} else if nodeArr[node1].depth < nodeArr[node2].depth {
-		for i := 0; i < nodeArr[node2].depth-nodeArr[node1].depth; i++ {
-			newNode2 = nodeArr[newNode2].parentNode
+	if depthArr[node1] > depthArr[node2] {
+		newNode1, newNode2 = node2, node1
+	}
+
+	for i := 19; i >= 0; i-- {
+		sub := depthArr[newNode2] - depthArr[newNode1]
+
+		// 비트연산을 통해 깊이 차를 빠르게 구함
+		if sub >= (1 << i) {
+			newNode2 = parentArr[newNode2][i]
 		}
 	}
 
-	return newNode1, newNode2
+	if newNode1 == newNode2 {
+		return newNode1
+	} else {
+		// LCA를 구할 때까지 반복
+		for i := 19; i >= 0; i-- {
+			if parentArr[newNode1][i] != parentArr[newNode2][i] {
+				newNode1 = parentArr[newNode1][i]
+				newNode2 = parentArr[newNode2][i]
+			}
+		}
+
+		return parentArr[newNode1][0]
+	}
 }
