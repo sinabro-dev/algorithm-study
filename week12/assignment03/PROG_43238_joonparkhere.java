5f67e6b8f7363973494dabde9f34600aa4a0cd56
joonparkhere <tmdgh0221@gmail.com>
2021-10-07 18:07:55 +0900

    code: Add PROG 43238 Java

---
+++ week12/assignment03/PROG_43238_joonparkhere.java
@@ -0,0 +1,12 @@
+package assignment03;
+
+import java.util.*;
+
+public class PROG_43238_joonparkhere {
+
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+
+        return 0;
+    }
+}
