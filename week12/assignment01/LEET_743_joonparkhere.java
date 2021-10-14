a1559cef7722ebe93fc7b57b7f9e542c2ba0d470
joonparkhere <tmdgh0221@gmail.com>
2021-10-14 22:28:36 +0900

    code: Refactor LEET 743 Java

--- week12/assignment01/LEET_743_joonparkhere.java
+++ week12/assignment01/LEET_743_joonparkhere.java
@@ -1,4 +1,4 @@
-package assignment02;
+package assignment01;
 
 import java.util.*;
 
@@ -6,51 +6,40 @@ public class LEET_743_joonparkhere {
 
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
+
         for (int[] edge : times) {
-            if (!graph.containsKey(edge[0])) {
+            if (!graph.containsKey(edge[0]))
                 graph.put(edge[0], new ArrayList<>());
-            }
 
             graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
         }
 
-        PriorityQueue<int[]> heap = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
-        heap.offer(new int[]{0, k});
-
-        Map<Integer, Integer> distanceMap = new HashMap<>();
+        Map<Integer, Integer> delayMap = new HashMap<>();
+        PriorityQueue<int[]> heap = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]); // sorted ascending distance
+        heap.offer(new int[]{0, k});    // {distance, node}
 
         while (!heap.isEmpty()) {
             int[] info = heap.poll();
-            int distance = info[0];
-            int cur = info[1];
+            int delay = info[0], cur = info[1];
 
-            if (distanceMap.containsKey(cur)) {
-                continue;
-            }
-            distanceMap.put(cur, distance);
+            if (delayMap.containsKey(cur)) continue;
+            delayMap.put(cur, delay);
 
-            if (!graph.containsKey(cur)) {
-                continue;
-            }
+            if (!graph.containsKey(cur)) continue;
 
             for (int[] edge : graph.get(cur)) {
-                int next = edge[0];
-                int weight = edge[1];
+                int next = edge[0], weight = edge[1];
 
-                if (!distanceMap.containsKey(next)) {
-                    heap.offer(new int[]{distance + weight, next});
-                }
+                if (!delayMap.containsKey(next))
+                    heap.offer(new int[]{delay + weight, next});
             }
         }
 
-        if (distanceMap.size() != n) {
-            return -1;
-        }
+        int result = -1;
+        if (delayMap.size() == n)
+            for (int delay : delayMap.values())
+                result = Math.max(result, delay);
 
-        int result = 0;
-        for (int distance : distanceMap.values()) {
-            result = Math.max(result, distance);
-        }
         return result;
     }
 
