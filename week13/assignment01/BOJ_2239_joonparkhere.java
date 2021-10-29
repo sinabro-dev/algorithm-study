c95609cae9976a9d46f091ab03ccd3a2fa505839
joonparkhere <tmdgh0221@gmail.com>
2021-10-29 21:37:34 +0900

    code: Apply bit operation BOJ 2239 Java

--- week13/assignment01/BOJ_2239_joonparkhere.java
+++ week13/assignment01/BOJ_2239_joonparkhere.java
@@ -11,20 +11,13 @@ public class BOJ_2239_joonparkhere {
 
     static int[][] board = new int[SUDOKU_SIZE][SUDOKU_SIZE];
     static List<int[]> candidateList = new ArrayList<>();
-    static boolean[][] isBlockExist = new boolean[SUDOKU_SIZE][SUDOKU_SIZE + 1];
-    static boolean[][] isRowExist = new boolean[SUDOKU_SIZE][SUDOKU_SIZE + 1];
-    static boolean[][] isColExist = new boolean[SUDOKU_SIZE][SUDOKU_SIZE + 1];
+    static int[] isRowExist = new int[SUDOKU_SIZE];
+    static int[] isColExist = new int[SUDOKU_SIZE];
+    static int[] isBlockExist = new int[SUDOKU_SIZE];
 
     public static void main(String[] args) throws IOException {
         init();
         check(0);
-    }
-
-    private static void updateExist(int row, int col, int value) {
-        int blockIdx = 3 * (row / 3) + (col / 3);
-        isBlockExist[blockIdx][value] = !isBlockExist[blockIdx][value];
-        isRowExist[row][value] = !isRowExist[row][value];
-        isColExist[col][value] = !isColExist[col][value];
     }
 
     static void init() throws IOException {
@@ -32,13 +25,15 @@ public class BOJ_2239_joonparkhere {
             String inputStr = br.readLine();
 
             for (int col = 0; col < SUDOKU_SIZE; col++) {
-                int value = inputStr.charAt(col) - '0';
-                board[row][col] = value;
+                board[row][col] = inputStr.charAt(col) - '0';
 
-                if (value == 0) {
+                if (board[row][col] == 0) {
                     candidateList.add(new int[]{row, col});
                 } else {
-                    updateExist(row, col, value);
+                    int digit = 1 << board[row][col];
+                    isRowExist[row] |= digit;
+                    isColExist[col] |= digit;
+                    isBlockExist[3 * (row / 3) + (col / 3)] |= digit;
                 }
             }
         }
@@ -50,33 +45,28 @@ public class BOJ_2239_joonparkhere {
             System.exit(0);
         }
 
-        int[] candidate = candidateList.get(idx);
-        int row = candidate[0], col = candidate[1];
+        int row = candidateList.get(idx)[0];
+        int col = candidateList.get(idx)[1];
+        int curDigit = isRowExist[row] | isColExist[col] | isBlockExist[3 * (row / 3) + (col / 3)];
 
         for (int value = 1; value <= SUDOKU_SIZE; value++) {
-            if (!isValid(row, col, value))
+            int nextDigit = 1 << value;
+
+            if ((curDigit & nextDigit) != 0)
                 continue;
 
-            updateExist(row, col, value);
             board[row][col] = value;
+            isRowExist[row] |= nextDigit;
+            isColExist[col] |= nextDigit;
+            isBlockExist[3 * (row / 3) + (col / 3)] |= nextDigit;
 
             check(idx + 1);
 
-            updateExist(row, col, value);
             board[row][col] = 0;
+            isRowExist[row] &= ~nextDigit;
+            isColExist[col] &= ~nextDigit;
+            isBlockExist[3 * (row / 3) + (col / 3)] &= ~nextDigit;
         }
-    }
-
-    private static boolean isValid(int row, int col, int value) {
-        int blockIdx = 3 * (row / 3) + (col / 3);
-        if (isBlockExist[blockIdx][value])
-            return false;
-        if (isRowExist[row][value])
-            return false;
-        if (isColExist[col][value])
-            return false;
-
-        return true;
     }
 
     private static void print() throws IOException {
