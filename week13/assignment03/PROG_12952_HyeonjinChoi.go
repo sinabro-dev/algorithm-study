3ccb50e376e913836b496a91da5150cf8c44ee7e
HyeonjinChoi <63872787+HyeonjinChoi@users.noreply.github.com>
2021-11-04 17:55:37 +0900

    code: Add PROG 12952

--- week13/assignment03/PROG_12952_HyeonjinChoi.go
+++ week13/assignment03/PROG_12952_HyeonjinChoi.go
@@ -1,1 +1,37 @@
+import "math"
 
+var (
+	flag  int
+	ans   int = 0
+	board []int
+)
+
+func solution(n int) int {
+    board = make([]int, n+1)
+	findAnswer(n, 1, 0)
+	return ans
+}
+
+func findAnswer(num, cur, queenNum int) {
+	if queenNum == num {
+		ans++
+		return
+	}
+
+	for i := 1; i <= num; i++ {
+		flag = 1
+
+		for j := 1; j < cur; j++ {
+			if board[j] == i || math.Abs(float64(board[j]-i)) == math.Abs(float64(j-cur)) {
+				flag = 0
+				break
+			}
+		}
+
+		if flag == 1 {
+			board[cur] = i
+			findAnswer(num, cur+1, queenNum+1)
+			board[cur] = 0
+		}
+	}
+}
