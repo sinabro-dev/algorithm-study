c2ee259292773fc53df763a8bf0d71f81d306dad
Seungho Park <sinabro2dev@gmail.com>
2023-06-12 00:07:20 +0900

    Solve leetcode 1146

---
+++ leetcode/LEET_1146.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode.com/problems/snapshot-array/description/
+
+Implement a SnapshotArray that supports the following interface:
+- SnapshotArray(int length) initializes an array-like data structure with the given length. Initially, each element equals 0.
+- void set(index, val) sets the element at the given index to be equal to val.
+- int snap() takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1.
+- int get(index, snap_id) returns the value at the given index, at the time we took the snapshot with the given snap_id.
+
+Input: ["SnapshotArray","set","snap","set","get"]
+[[3],[0,5],[],[0,6],[0,0]]
+Output: [null,null,0,null,5]
+Explanation: 
+SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
+snapshotArr.set(0,5);  // Set array[0] = 5
+snapshotArr.snap();  // Take a snapshot, return snap_id = 0
+snapshotArr.set(0,6);
+snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+"""
+class SnapshotArray:
+    """
+    Keyword: Binary Search, Design
+    Space: O(#SetCalled)
+    Time: O(log #SnapCalled)
+    """
+
+    def __init__(self, length: int):
+        self.snapshots = [[[-1 ,0]] for _ in range(length)]
+        self.id = 0
+
+    def set(self, index: int, val: int) -> None:
+        self.snapshots[index].append([self.id, val])
+
+    def snap(self) -> int:
+        self.id += 1
+        return self.id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        pos = bisect.bisect(self.snapshots[index], [snap_id + 1]) - 1
+        return self.snapshots[index][pos][1]
